7402c49f8a9df1f153556bf64f0553ffebc94bfc
rax <rakhafariaziz123@gmail.com>
2026-05-11 11:40:04 +0700

    membuat branch baru 'branch1'

--- pertemuan 9 (2).R
+++ pertemuan 9 (2).R
@@ -4,3 +4,8 @@ sd(mtcars$mpg)
 mean(mtcars$mpg)
 plot(mtcars$mpg, mtcars$hp, col="blue")
 plot(mtcars$mpg, mtcars$hp, col="red")
+
+#ganti branch baru, tulis di terminal :
+# git checkout -b nama-branch-baru
+
+
